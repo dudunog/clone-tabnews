7972399a4c07fbeb01031dd57934ab762cfe070c
Eduardo Nogueira <57373980+dudunog@users.noreply.github.com>
2023-12-10 16:24:57 +0000

    feat: add in construction page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
-  return <h1>Vamos programar em JavaScript??</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        height: "100vh",
+        justifyContent: "center",
+      }}
+    >
+      <h1 style={{ margin: 0 }}>Em construção...</h1>
+    </div>
+  );
 }
 
 export default Home;
